2df266b2cea5921efef17956e4092dc1fda6263f
Do Xuan Long <longdo@dobtech.vn>
2023-08-09 19:12:12 +0700

    handle navigation

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,12 +1,28 @@
 import React, { useEffect, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import { mount } from 'marketing/MarketingApp';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        // get the current path from the browser
+        const { pathname } = history.location;
+
+        // if the current path is not the same as the new path
+        // update the browser path
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
 import App from './App';
 
 // create mount function to mount the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    // listen to the history object for changes
+    // and call onNavigate with the new location
+    // whenever the location changes
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // if in dev mode and running in isolation,
@@ -13,7 +34,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
