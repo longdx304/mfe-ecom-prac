a9d71b0c3a301658c4df9a640b685786a6f03114
Do Xuan Long <longdo@dobtech.vn>
2023-08-09 22:33:28 +0700

    added auth bootstrap

---
+++ auth/src/bootstrap.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
+import App from './App';
+
+// create mount function to mount the app
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    // listen to the history object for changes
+    // and call onNavigate with the new location
+    // whenever the location changes
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// if in dev mode and running in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+// if in production mode, export mount for container to call
+export { mount };
